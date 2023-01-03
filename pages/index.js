89f11c7b38fc0e5a890ea6224404412704f5d18b
Usama Saleem <usama.saleem@hotmail.com>
2023-01-03 09:48:20 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -44,17 +44,17 @@ export default function Home() {
       <main className={styles.main}>
         <img src="/imam.png" className={styles.icon} />
         <h3>Ask Imam AI</h3>
-        <p style={
-          {
+        <p style={{
             padding: "0px",
           }
         }>Made by Usama</p>
-        <p style={
-          {
+        <p style={{
             paddingBottom: "40px",
             fontSize : "12px",
           }
-        }>finna put zakir naik out of a job</p>
+            }>finna put zakir naik out of a job</p>
+
+
         <form onSubmit={onSubmit}>
           <input 
           
--- pages/loadingicon.js
+++ pages/loadingicon.js
@@ -6,4 +6,10 @@ function LoadingIcon() {
   );
 }
 
+function zakir(){
+    return (
+         <img src="https://media.tenor.com/Ps5-jSncJ0UAAAAM/zakir-naik-may-allah-forgive-you.gif" width="150" height="80" />
+    );
+}
+
 export default LoadingIcon;
